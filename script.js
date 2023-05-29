03cdab5376c2f491b61798ad1bc20200ac78f1ec
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-05-29 15:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,34 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const promises = [
+  new Promise((resolve) => setTimeout(() => resolve('Promise 1'), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 2'), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 3'), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 4'), getRandomTime())),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 5'), getRandomTime())),
+];
+
+// Helper function to generate random time between 1 and 5 seconds
+function getRandomTime() {
+  return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+}
+
+// Promise.any to wait for the first promise to resolve
+Promise.any(promises)
+  .then((result) => {
+    // Print the result to the output div
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = result;
+  })
+  .catch((error) => {
+    // Handle any errors
+    console.error(error);
+  });
+
+
+
+
+
+
+
